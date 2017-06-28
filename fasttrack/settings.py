4464a458f29eddfdf4e99c3403c41813670bcc77
Carsten Knoblich <carsten@staticline.de>
2017-06-28 15:20:05 +0200

    configured static file handling

--- fasttrack/settings.py
+++ fasttrack/settings.py
@@ -106,6 +106,8 @@ LOGIN_REDIRECT_URL = 'store:home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
